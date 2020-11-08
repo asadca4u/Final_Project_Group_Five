2547907bfeccf48889b1eedf4ccc49c746232fa2
Arthur Kasozi <arkasozi@gmail.com>
2020-11-08 10:02:11 -0500

    adding

---
+++ .sql
@@ -0,0 +1,20 @@
+CREATE TABLE diabetes_data_upload (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+

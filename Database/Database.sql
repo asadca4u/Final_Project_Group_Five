c5b78b1ce6701626d46c02460b63b3fe8474dd84
Arthur Kasozi <arkasozi@gmail.com>
2020-11-22 13:26:31 -0500

    adding

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,143 +1,210 @@
-CREATE TABLE all_data (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+-- Table: public.all_data
 
-CREATE TABLE female_data (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+-- DROP TABLE public.all_data;
 
-CREATE TABLE male_data (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+CREATE TABLE public.all_data
+(
+    "Age" bigint,
+    "Gender" bigint,
+    "Polyuria" bigint,
+    "Polydipsia" bigint,
+    "sudden weight loss" bigint,
+    weakness bigint,
+    "Polyphagia" bigint,
+    "Genital thrush" bigint,
+    "visual blurring" bigint,
+    "Itching" bigint,
+    "Irritability" bigint,
+    "delayed healing" bigint,
+    "partial paresis" bigint,
+    "muscle stiffness" bigint,
+    "Alopecia" bigint,
+    "Obesity" bigint,
+    class bigint
+)
 
-CREATE TABLE diabetes_data_upload (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+TABLESPACE pg_default;
 
-CREATE TABLE generation_one (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+ALTER TABLE public.all_data
+    OWNER to postgres;
+    
+-- Table: public.diabetes_data_upload
 
-CREATE TABLE generation_two (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+-- DROP TABLE public.diabetes_data_upload;
 
-CREATE TABLE generation_three (
-  Age varchar,
-  Gender varchar,
-  Polyuria varchar,
-  Polydipsia varchar,
-  sudden_weight_loss varchar,
-  weakness varchar,
-  Polyphagia varchar,
-  Genital_thrush varchar,
-  visual_blurring varchar,
-  Itching varchar,
-  Irritability varchar,
-  delayed_healing varchar,
-  partial_paresis varchar,
-  muscle_stiffness varchar,
-  Alopecia varchar,
-  Obesity varchar,
-  class varchar
-);
+CREATE TABLE public.diabetes_data_upload
+(
+    age character varying COLLATE pg_catalog."default",
+    gender character varying COLLATE pg_catalog."default",
+    polyuria character varying COLLATE pg_catalog."default",
+    polydipsia character varying COLLATE pg_catalog."default",
+    sudden_weight_loss character varying COLLATE pg_catalog."default",
+    weakness character varying COLLATE pg_catalog."default",
+    polyphagia character varying COLLATE pg_catalog."default",
+    genital_thrush character varying COLLATE pg_catalog."default",
+    visual_blurring character varying COLLATE pg_catalog."default",
+    itching character varying COLLATE pg_catalog."default",
+    irritability character varying COLLATE pg_catalog."default",
+    delayed_healing character varying COLLATE pg_catalog."default",
+    partial_paresis character varying COLLATE pg_catalog."default",
+    muscle_stiffness character varying COLLATE pg_catalog."default",
+    alopecia character varying COLLATE pg_catalog."default",
+    obesity character varying COLLATE pg_catalog."default",
+    class character varying COLLATE pg_catalog."default"
+)
 
+TABLESPACE pg_default;
 
+ALTER TABLE public.diabetes_data_upload
+    OWNER to postgres;
+
+-- Table: public.female_data
+
+-- DROP TABLE public.female_data;
+
+CREATE TABLE public.female_data
+(
+    "Age" bigint,
+    "Gender" bigint,
+    "Polyuria" bigint,
+    "Polydipsia" bigint,
+    "sudden weight loss" bigint,
+    weakness bigint,
+    "Polyphagia" bigint,
+    "Genital thrush" bigint,
+    "visual blurring" bigint,
+    "Itching" bigint,
+    "Irritability" bigint,
+    "delayed healing" bigint,
+    "partial paresis" bigint,
+    "muscle stiffness" bigint,
+    "Alopecia" bigint,
+    "Obesity" bigint,
+    class bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.female_data
+    OWNER to postgres;
+
+-- Table: public.generation_one
+
+-- DROP TABLE public.generation_one;
+
+CREATE TABLE public.generation_one
+(
+    "Age" bigint,
+    "Gender" bigint,
+    "Polyuria" bigint,
+    "Polydipsia" bigint,
+    "sudden weight loss" bigint,
+    weakness bigint,
+    "Polyphagia" bigint,
+    "Genital thrush" bigint,
+    "visual blurring" bigint,
+    "Itching" bigint,
+    "Irritability" bigint,
+    "delayed healing" bigint,
+    "partial paresis" bigint,
+    "muscle stiffness" bigint,
+    "Alopecia" bigint,
+    "Obesity" bigint,
+    class bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.generation_one
+    OWNER to postgres;
+
+-- Table: public.generation_three
+
+-- DROP TABLE public.generation_three;
+
+CREATE TABLE public.generation_three
+(
+    "Age" bigint,
+    "Gender" bigint,
+    "Polyuria" bigint,
+    "Polydipsia" bigint,
+    "sudden weight loss" bigint,
+    weakness bigint,
+    "Polyphagia" bigint,
+    "Genital thrush" bigint,
+    "visual blurring" bigint,
+    "Itching" bigint,
+    "Irritability" bigint,
+    "delayed healing" bigint,
+    "partial paresis" bigint,
+    "muscle stiffness" bigint,
+    "Alopecia" bigint,
+    "Obesity" bigint,
+    class bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.generation_three
+    OWNER to postgres;
+
+-- Table: public.generation_two
+
+-- DROP TABLE public.generation_two;
+
+CREATE TABLE public.generation_two
+(
+    "Age" bigint,
+    "Gender" bigint,
+    "Polyuria" bigint,
+    "Polydipsia" bigint,
+    "sudden weight loss" bigint,
+    weakness bigint,
+    "Polyphagia" bigint,
+    "Genital thrush" bigint,
+    "visual blurring" bigint,
+    "Itching" bigint,
+    "Irritability" bigint,
+    "delayed healing" bigint,
+    "partial paresis" bigint,
+    "muscle stiffness" bigint,
+    "Alopecia" bigint,
+    "Obesity" bigint,
+    class bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.generation_two
+    OWNER to postgres;
+-- Table: public.male_data
+
+-- DROP TABLE public.male_data;
+
+CREATE TABLE public.male_data
+(
+    "Age" bigint,
+    "Gender" bigint,
+    "Polyuria" bigint,
+    "Polydipsia" bigint,
+    "sudden weight loss" bigint,
+    weakness bigint,
+    "Polyphagia" bigint,
+    "Genital thrush" bigint,
+    "visual blurring" bigint,
+    "Itching" bigint,
+    "Irritability" bigint,
+    "delayed healing" bigint,
+    "partial paresis" bigint,
+    "muscle stiffness" bigint,
+    "Alopecia" bigint,
+    "Obesity" bigint,
+    class bigint
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.male_data
+    OWNER to postgres;                    
 
 

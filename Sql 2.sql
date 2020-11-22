aca62cbce820022e5aae5c40fc7572b6dc8c0c6b
Arthur Kasozi <arkasozi@gmail.com>
2020-11-22 03:38:08 -0500

    adding

---
+++ Sql 2.sql
@@ -0,0 +1,143 @@
+CREATE TABLE all_data (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+CREATE TABLE female_data (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+CREATE TABLE male_data (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+CREATE TABLE diabetes_data_upload (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+CREATE TABLE generation_one (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+CREATE TABLE generation_two (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+CREATE TABLE generation_three (
+  Age varchar,
+  Gender varchar,
+  Polyuria varchar,
+  Polydipsia varchar,
+  sudden_weight_loss varchar,
+  weakness varchar,
+  Polyphagia varchar,
+  Genital_thrush varchar,
+  visual_blurring varchar,
+  Itching varchar,
+  Irritability varchar,
+  delayed_healing varchar,
+  partial_paresis varchar,
+  muscle_stiffness varchar,
+  Alopecia varchar,
+  Obesity varchar,
+  class varchar
+);
+
+);
+
+

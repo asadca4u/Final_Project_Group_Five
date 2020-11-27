8cf75ebfc4033c11377b4ec94f1db8c88d47092f
Asad Khan <asad_ca4u@hotmail.com>
2020-11-26 19:36:08 -0500

    completed all the input fields

---
+++ Dashboard/app.js
@@ -0,0 +1,162 @@
+function collect_data()
+{
+    let age = document.getElementById("age").value;
+    console.log("Age: "+age)
+
+    //import height and weight, calculate BMI then classify as overweight or not
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+    let BMI = 703*(weight/(height**2));
+    //define variable that will hold status of being overweight. 
+    let overweight;
+    //console.log(BMI);
+    if (BMI < 30)
+        overweight = 0;
+    else
+        overweight = 1;
+    console.log("Overweight: "+overweight);
+
+    //import gender and encode as a variable
+    let male = document.getElementById("male");
+    let female = document.getElementById("female");
+    let gender;
+    if(male.checked==true)
+        gender = male.value;
+    else 
+        gender = female.value;
+    console.log("Gender: "+gender);
+
+    //import polyuria and encode as a variable
+    let yes1 = document.getElementById("yes1");
+    let polyuria;
+    if(yes1.checked==true)
+        polyuria = 1;
+    else 
+        polyuria = 0;
+    console.log("Polyuria: "+polyuria);
+    document.getElementById("yes1").reset;
+    document.getElementById("no1").reset;
+
+
+    //import polydipsia and encode as a variable
+    let yes2 = document.getElementById("yes2");
+    let polydipsia;
+    if(yes2.checked==true)
+        polydipsia = 1;
+    else 
+        polydipsia = 0;
+    console.log("Polydipsia: "+polydipsia);
+
+    //import polydipsia and encode as a variable
+    let yes3 = document.getElementById("yes3");
+    let weight_loss;
+    if(yes3.checked==true)
+        weight_loss = 1;
+    else 
+        weight_loss = 0;
+    console.log("Weight Loss: "+weight_loss);
+
+    //import irritability and encode as a variable
+    let yes4 = document.getElementById("yes4");
+    let irritability;
+    if(yes3.checked==true)
+        irritability = 1;
+    else 
+        irritability = 0;
+    console.log("Irritability: "+irritability);
+
+    //import paresis and encode as a variable
+    let yes5 = document.getElementById("yes5");
+    let paresis;
+    if(yes5.checked==true)
+        paresis = 1;
+    else 
+        paresis = 0;
+    console.log("Paresis: "+paresis);
+
+    //import polyphagia and encode as a variable
+    let yes6 = document.getElementById("yes6");
+    let polyphagia;
+    if(yes6.checked==true)
+        polyphagia = 1;
+    else 
+        polyphagia = 0;
+    console.log("polyphagia: "+polyphagia);
+
+     //import alopecia and encode as a variable
+    let yes7 = document.getElementById("yes7");
+    let alopecia;
+    if(yes7.checked==true)
+        alopecia = 1;
+    else 
+        alopecia = 0;
+    console.log("Alopecia: "+alopecia);
+
+     //import vision and encode as a variable
+    let yes8 = document.getElementById("yes8");
+    let vision;
+    if(yes8.checked==true)
+        vision = 1;
+    else 
+        vision = 0;
+    console.log("Visual Blurring: "+vision);
+
+    //import delayed healing and encode as a variable
+    let yes9 = document.getElementById("yes9");
+    let healing;
+    if(yes9.checked==true)
+        healing = 1;
+    else 
+        healing = 0;
+    console.log("Delayed Healing: "+healing);
+
+    //import itching and encode as a variable
+    let yes10 = document.getElementById("yes10");
+    let itching;
+    if(yes10.checked==true)
+        itching = 1;
+    else 
+        itching = 0;
+    console.log("Itching: "+itching);
+
+    //import stiffness and encode as a variable
+    let yes11 = document.getElementById("yes11");
+    let stiffness;
+    if(yes11.checked==true)
+        stiffness = 1;
+    else 
+        stiffness = 0;
+    console.log("Stiffness: "+stiffness);
+
+    //import thrush and encode as a variable
+    let yes12 = document.getElementById("yes12");
+    let thrush;
+    if(yes12.checked==true)
+        thrush = 1;
+    else 
+        thrush = 0;
+    console.log("Genital Thrush: "+thrush);
+
+    //import weakness and encode as a variable
+    let yes13 = document.getElementById("yes13");
+    let weakness;
+    if(yes13.checked==true)
+        weakness = 1;
+    else 
+        weakness = 0;
+    console.log("General Weakness: "+weakness);
+      
+    //use forloop to reset all input fields after submission
+    var myRadioList = document.getElementsByTagName("input");
+        for (i = 0; i < myRadioList.length; i++) 
+        {
+            if (myRadioList[i].type == "radio")
+                {
+                    myRadioList[i].checked = false; 
+                }
+            else if (myRadioList[i].type == "number")
+                {
+                    myRadioList[i].value = "";
+                }
+        }
+}
